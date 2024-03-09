bcfe88648a80700aa445cd77ddf281119a5f5f5b
tmfrl1590 <tmfrl1590@gmail.com>
2024-03-09 23:22:28 +0900

    #1 프로젝트 설정

---
+++ shared/src/commonMain/kotlin/com/json/daliypulse/Platform.kt
@@ -0,0 +1,12 @@
+package com.json.daliypulse
+
+expect class Platform {
+    val osName: String
+    val osVersion: String
+    val deviceModel: String
+    val density: Int
+
+
+    fun logSystemInfo()
+}
+
